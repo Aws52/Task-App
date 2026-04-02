c9886542081faf483c78526c2956075ef2c17208
Aws52 <aws2004damra@gmail.com>
2026-04-02 14:17:33 +0300

    added 'TaskStatus' and 'TaskPriority' enums

---
+++ task/src/main/java/com/aws/task/domain/enitiy/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.aws.task.domain.enitiy;
+
+// TaskPriority enum to model task priority (HIGH, MEDIUM, LOW)
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ task/src/main/java/com/aws/task/domain/enitiy/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.aws.task.domain.enitiy;
+
+// TaskStatus enum to model task status (OPEN, COMPLETE)
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
