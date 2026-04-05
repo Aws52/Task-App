af0ea53d504742c64d00dddd92598e241d3f66e7
Aws52 <aws2004damra@gmail.com>
2026-04-05 17:26:59 +0300

    defined TaskMapper interface and implemented it

---
+++ task/src/main/java/com/aws/task/mapper/TaskMapper.java
@@ -0,0 +1,14 @@
+package com.aws.task.mapper;
+
+import com.aws.task.domain.CreateTaskRequest;
+import com.aws.task.domain.dto.CreateTaskRequestDto;
+import com.aws.task.domain.dto.TaskDto;
+import com.aws.task.domain.enitiy.Task;
+
+public interface TaskMapper {
+
+    CreateTaskRequest fromDto(CreateTaskRequestDto dto);
+
+    TaskDto toDto(Task task);
+
+}
---
+++ task/src/main/java/com/aws/task/mapper/impl/TaskMapperImpl.java
@@ -0,0 +1,36 @@
+package com.aws.task.mapper.impl;
+
+import com.aws.task.domain.CreateTaskRequest;
+import com.aws.task.domain.dto.CreateTaskRequestDto;
+import com.aws.task.domain.dto.TaskDto;
+import com.aws.task.domain.enitiy.Task;
+import com.aws.task.mapper.TaskMapper;
+
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public CreateTaskRequest fromDto(CreateTaskRequestDto dto) {
+        return new CreateTaskRequest(
+            dto.title(),
+            dto.description(),
+            dto.dueDate(),
+            dto.priority()
+        );
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+        if (task == null) {
+            return null;
+        }
+        return new TaskDto(
+            task.getId(),
+            task.getTitle(),
+            task.getDescription(),
+            task.getDueDate(),
+            task.getPriority(),
+            task.getStatus()
+        );
+    }
+
+}
