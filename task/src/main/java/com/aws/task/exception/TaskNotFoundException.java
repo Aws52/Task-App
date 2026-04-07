ac58f89388c8e0594ea1954cc151d37960956e4c
Aws52 <aws2004damra@gmail.com>
2026-04-07 14:18:46 +0300

    added TaskNotFoundException class

---
+++ task/src/main/java/com/aws/task/exception/TaskNotFoundException.java
@@ -0,0 +1,20 @@
+package com.aws.task.exception;
+
+import java.util.UUID;
+
+public class TaskNotFoundException extends RuntimeException {
+
+    private final UUID id;
+
+    public TaskNotFoundException(UUID id) {
+        super(
+            String.format("Task with id %s not found", id)
+        );
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+}
