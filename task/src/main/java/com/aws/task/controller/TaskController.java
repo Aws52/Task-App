c62b47439ec602311d85f4fe546d228b2f11bd48
Aws52 <aws2004damra@gmail.com>
2026-04-07 14:43:29 +0300

    Implemeted the UpdateTask method on the TaskController

--- task/src/main/java/com/aws/task/controller/TaskController.java
+++ task/src/main/java/com/aws/task/controller/TaskController.java
@@ -1,7 +1,9 @@
 package com.aws.task.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aws.task.domain.CreateTaskRequest;
+import com.aws.task.domain.UpdateTaskRequest;
 import com.aws.task.domain.dto.CreateTaskRequestDto;
 import com.aws.task.domain.dto.TaskDto;
+import com.aws.task.domain.dto.UpdateTaskRequestDto;
 import com.aws.task.domain.entity.Task;
 import com.aws.task.mapper.TaskMapper;
 import com.aws.task.service.TaskService;
@@ -18,7 +22,9 @@ import com.aws.task.service.TaskService;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -50,4 +56,16 @@ public class TaskController {
         List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
         return ResponseEntity.ok(taskDtos);
     }
+
+    @PutMapping(path = "/{taskId}")
+    public ResponseEntity<TaskDto> updateTask(
+        @PathVariable UUID taskId,
+        @Valid @RequestBody UpdateTaskRequestDto updateTaskRequestDto
+    ) {
+        UpdateTaskRequest updateTaskRequest = taskMapper.fromDto(updateTaskRequestDto);
+        Task task = taskService.updateTask(taskId, updateTaskRequest);
+        TaskDto TaskDto = taskMapper.toDto(task);
+        return ResponseEntity.ok(TaskDto);
+    }
+
 }
